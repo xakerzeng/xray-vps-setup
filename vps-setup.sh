3a50819e87d79aa4b01534f8ebb8e14b07fb1888
Akiyamov <akiyamov@Bakloferon>
2024-11-17 19:05:45 +0500

    Add input drop

--- vps-setup.sh
+++ vps-setup.sh
@@ -51,13 +51,11 @@ systemctl restart xray
 systemctl restart caddy
 
 # Configure iptables
-iptables --delete-chain
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -P INPUT DROP
 iptables-save > /etc/network/iptables.rules
 
 # Prettyprint outbound and clipboard string
