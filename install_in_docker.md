4772eb0af8d8f54f242a84a8a1cd295f9f62c7ed
Akiyamov <116005851+Akiyamov@users.noreply.github.com>
2025-01-21 05:47:56 +0500

    Marzban added, WARP added, disabling iptables/ssh/new user configure added (#7)

---
+++ install_in_docker.md
@@ -0,0 +1,356 @@
+<h1 align="center">VLESS + Reality Self Steal в Docker</h2>
+
+### Что потребуется:
+- VPS 
+- Свой домен
+
+В статье будет рассмотрена установка как чистого Xray, так и Marzban.  
+
+## Настройка сервера
+
+### Настройка SSH
+
+На своем ПК, неважно, GNU/Linux или Windows. __На Windows используйте Powershell__. Открываем терминал и выполняем следующую команду:
+```bash
+ssh-keygen -t ed25519
+```
+После выполнения команды вам предложат изменить место хранения ключа и добавить пароль к нему. Менять локацию не надо, пароль же можете добавить ради безопасности.
+Создав ключ, вам будет выведена локация публичной и приватной его части, нам нужно перекинуть публичную часть этого ключа на нашу VPS.  
+На Linux:
+```bash
+ssh-copy-id -i ~/.ssh/id_ed25519.pub ваш_пользователь@ваша_vps
+```
+На Windows:
+```powershell
+ssh-copy-id -i $env:USERPROFILE\.ssh\id_ed25519.pub ваш_пользователь@ваша_vps
+```
+Если данная команда у вас не сработала на Windows, то нужно выполнить следующую:
+```powershell
+type $env:USERPROFILE\.ssh\id_ed25519.pub | ssh ваш_пользователь@ваша_vps "cat >> .ssh/authorized_keys"
+```
+__Далее все делается на VPS.__  
+Для отключения входа по паролю выполняем следующую команду:
+```bash
+grep -r PasswordAuthentication /etc/ssh -l | xargs -n 1 sed -i -e "/PasswordAuthentication /c\PasswordAuthentication no"
+```
+Сделав это можно перезапустить SSH. 
+```bash
+sudo systemctl restart ssh
+```
+
+### Настройки iptables
+Нам нужно оставить открытыми порты для SSH, 80(HTTP) и 443(HTTPS).
+Для этого нужно выполнить следующие команды:
+```bash
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT 
+iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -P INPUT DROP
+iptables-save > /etc/network/iptables.rules
+```
+
+### Включение BBR
+Достаточно выполнить следующие команды:
+```bash
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+```
+
+## Создание прокси
+
+### Установка Docker
+Для установки нужно выполнить следующую команду:
+```bash
+bash <(wget -qO- https://get.docker.com) @ -o get-docker.sh
+```
+Если вы работаете не от админа, то выполните следующие команды, чтобы не писать `sudo` каждый раз:
+```bash
+sudo groupadd docker
+sudo usermod -aG docker $USER
+```
+
+### Получение данных для прокси
+В этой части будут описаны необходимые данные, а также способ их получения. Позже эти данные будут использованы в конфигурации.  
+- __VLESS_DOMAIN__: Ваш домен. Если используется punycode, то далее используется ТОЛЬКО на латинице.  
+- __XRAY_PBK+PIK__: `docker run --rm ghcr.io/xtls/xray-core x25519`
+Оба значения для нас важны, Public key = PBK, Private key = PIK.  
+- __XRAY_SID__: `openssl rand -hex 8`
+Short id, используется для различения разных клиентов  
+
+Следующие данные нужны только если вы будете устанавливать панель Marzban.  
+- __MARZBAN_USER__: `tr -dc A-Za-z0-9 </dev/urandom | head -c 8; echo`  
+Пользователь панели  
+- __MARZBAN_PASS__: `tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo`  
+Пароль пользователя панели  
+- __MARZBAN_PATH__: `openssl rand -hex 8`  
+URL панели  
+- __MARZBAN_SUB_PATH__: `openssl rand -hex 8`  
+URL подписок  
+
+### Настройка прокси
+Создадим папку `/opt/xray-vps-setup` командой `mkdir -p /opt/xray-vpx-setup`.  
+После этого переходим в папку и создаем в ней файл `docker-compose.yml`  
+
+<details>
+  <summary>Marzban</summary>  
+
+```yaml
+services:
+  caddy:
+    image: caddy:2.9
+    restart: always
+    network_mode: host
+    volumes:
+      - ./caddy/data:/data
+      - ./caddy/Caddyfile:/etc/caddy/Caddyfile
+      - ./marzban_lib:/run/marzban
+  marzban:
+    image: gozargah/marzban:v0.8.4
+    restart: always
+    env_file: ./marzban/.env
+    network_mode: host
+    volumes:
+      - ./marzban_lib:/var/lib/marzban
+      - ./marzban/xray_config.json:/code/xray_config.json
+      - ./marzban/templates:/var/lib/marzban/templates
+```  
+</details>
+<details>
+  <summary>Xray</summary>  
+
+```yaml
+services:
+  caddy:
+    image: caddy:2.9
+    restart: always
+    network_mode: host
+    volumes:
+      - ./caddy/data:/data
+      - ./caddy/Caddyfile:/etc/caddy/Caddyfile
+      - ./caddy/templates:/srv
+  xray:
+    image: ghcr.io/xtls/xray-core:25.1.1
+    restart: always
+    network_mode: host
+    volumes:
+      - ./xray:/etc/xray
+```  
+</details>
+Создаем папку `/opt/xray-vpx-setup/caddy` и в ней создаем файл `Caddyfile` и меняем его следующим образом.  
+<details><summary>Marzban</summary>
+
+```yaml
+{
+        https_port 4123
+        default_bind 127.0.0.1
+        servers {
+                listener_wrappers {
+                        proxy_protocol {
+                                allow 127.0.0.1/32
+                        }
+                        tls
+                }
+        }
+        auto_https disable_redirects
+}
+https://$VLESS_DOMAIN { 
+        reverse_proxy * unix//run/marzban/marzban.socket
+}
+http://$VLESS_DOMAIN {
+        bind 0.0.0.0
+        redir https://$VLESS_DOMAIN{uri} permanent
+}
+:4123 {
+        tls internal
+        respond 204
+}
+:80 {
+        bind 0.0.0.0
+        respond 204
+}
+```
+
+</details>
+<details><summary>Чистый Xray</summary>
+
+```yaml
+{
+        https_port 4123
+        default_bind 127.0.0.1
+        servers {
+                listener_wrappers {
+                        proxy_protocol {
+                                allow 127.0.0.1/32
+                        }
+                        tls
+                }
+        }
+        auto_https disable_redirects
+}
+https://$VLESS_DOMAIN {
+        root * /srv
+        file_server
+}
+http://$VLESS_DOMAIN {
+        bind 0.0.0.0
+        redir https://$VLESS_DOMAIN{uri} permanent
+}
+:4123 {
+        tls internal
+        respond 204
+}
+:80 {
+        bind 0.0.0.0
+        respond 204
+}
+```
+
+</details>
+Настроив caddy требуется добавить страницу для маскировки. Для xray и marzban команды отличаются:  
+Xray  
+
+```bash
+wget -qO- https://raw.githubusercontent.com/Jolymmiles/confluence-marzban-home/main/index.html  | envsubst > /opt/xray-vps-setup/caddy/templates/index.html
+```
+Marzban
+```bash
+wget -qO- https://raw.githubusercontent.com/Jolymmiles/confluence-marzban-home/main/index.html  | envsubst > /opt/xray-vps-setup/marzban/templates/home/index.html
+```
+
+После этого надо создать файл конфигурации Xray, если вы ставите marzban, то он будет находится в `/opt/xray-vps-setup/marzban/xray_config.json`, если чистый xray, то `/opt/xray-vps-setup/xray/config.json`  
+
+```json
+{
+  "log": {
+    "loglevel": "debug"
+  },
+  "inbounds": [
+    {
+      "tag": "VLESS TCP VISION REALITY",
+      "listen": "0.0.0.0",
+      "port": 443,
+      "protocol": "vless",
+      "settings": {
+        "clients": [
+          {
+            "id": "XRAY_UUDI", // ПОМЕНЯТЬ НА СВОЕ
+            "email": "default",
+            "flow": "xtls-rprx-vision"
+          }
+        ],
+        "decryption": "none"
+      },
+      "streamSettings": {
+        "network": "raw",
+        "security": "reality",
+        "realitySettings": {
+          "xver": 1,
+          "dest": "127.0.0.1:4123",
+          "serverNames": [
+            "VLESS_DOMAIN" // ПОМЕНЯТЬ НА СВОЕ
+          ],
+          "privateKey": "XRAY_PIK", // ПОМЕНЯТЬ НА СВОЕ
+          "shortIds": [
+            "XRAY_SID" // ПОМЕНЯТЬ НА СВОЕ
+          ]
+        }
+      },
+      "sniffing": {
+        "enabled": true,
+        "destOverride": [
+          "http",
+          "tls"
+        ],
+        "routeOnly": true
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "tag": "direct",
+      "settings": {
+        "domainStrategy": "UseIPv4"
+      }
+    },
+    {
+      "protocol": "blackhole",
+      "tag": "block"
+    }
+  ],
+  "routing": {
+    "rules": [
+      {
+        "protocol": "bittorrent",
+        "outboundTag": "block"
+      }
+    ],
+    "domainStrategy": "IPIfNonMatch"
+  },
+  "dns": {
+    "servers": [
+      "1.1.1.1",
+      "8.8.8.8"
+    ],
+    "queryStrategy": "UseIPv4",
+    "disableFallback": false,
+    "tag": "dns-aux"
+  }
+}
+```
+
+Для Marzban необходимо также добавить `.env` файл. Создайте файл `/opt/xray-vps-setup/marzban/.env` и вставьте следующее: 
+```
+SUDO_USERNAME = "xray_admin"
+SUDO_PASSWORD = "$MARZBAN_PASS"
+UVICORN_UDS = "/var/lib/marzban/marzban.socket"
+DASHBOARD_PATH = "/$MARZBAN_PATH/"
+XRAY_JSON = "xray_config.json"
+XRAY_SUBSCRIPTION_URL_PREFIX = "https://$VLESS_DOMAIN"
+XRAY_SUBSCRIPTION_PATH = "$MARZBAN_SUB_PATH"
+SQLALCHEMY_DATABASE_URL = "sqlite:////var/lib/marzban/db.sqlite3"
+CUSTOM_TEMPLATES_DIRECTORY="/var/lib/marzban/templates/"
+SUBSCRIPTION_PAGE_TEMPLATE="subscription/index.html"
+HOME_PAGE_TEMPLATE="home/index.html"
+```
+
+## Настройка WARP
+
+Для того, чтобы доабвить WARP для того, чтобы в Россию наш юзер ходил черзе него, то надо сделать следующее.  
+Устанавливаем WARP:
+```bash
+curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
+apt update 
+apt install cloudflare-warp -y
+```
+Настроим WARP:
+```bash
+warp-cli registration new
+warp-cli mode proxy
+warp-cli proxy port 40000
+warp-cli connect
+```
+Если на этом этапе ловим ошибку подключения, то не продолжайте, WARP не рабоатет.  
+Установка `yq`:
+```bash
+wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
+```
+Далее с помощью `yq` мы установим в уже существующий кофниг WARP:
+```bash
+yq eval '.outbounds[.outbounds | length ] |= . + {"tag": "warp","protocol": "socks","settings": {"servers": [{"address": "127.0.0.1","port": 40000}]}}' -i $XRAY_CONFIG_WARP
+yq eval '.routing.rules[.routing.rules | length ] |= . + {"outboundTag": "warp", "domain": ["geosite:category-ru"]}' -i $XRAY_CONFIG_WARP
+
+```
+Заменяем $XRAY_CONFIG_WARP на `/opt/xray-vps-setup/marzban/xray_config.json` для marzban и на `/opt/xray-vps-setup/xray/config.json` для чистого xray. После этого перезапускаем все:
+```bash
+docker compose -f /opt/xray-vps-setup/docker-compose.yml down && docker compose -f /opt/xray-vps-setup/docker-compose.yml up -d
+```
+
+#
+
+Если вы хотите помочь что-то исправить, добавить и тд, то делайте PR или пишите в [ТГ](https://t.me/Akiyamov).
--- vps-setup.sh
+++ vps-setup.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-export GIT_BRANCH="main"
-export GIT_REPO="$GIT_REPO"
+export GIT_BRANCH="marzban"
+export GIT_REPO="Akiyamov/xray-vps-setup"
 
 # Check if script started as root
 if [ "$EUID" -ne 0 ]
@@ -11,7 +11,7 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# Install idn
+# Install idn 
 apt-get update
 apt-get install idn -y
 
@@ -21,7 +21,7 @@ read -ep "Enter your domain:"$'\n' input_domain
 export VLESS_DOMAIN=$(echo $input_domain | idn)
 export TEST_DOMAIN=$(nslookup $VLESS_DOMAIN | awk -F': ' 'NR==6 { print $2 } ')
 if [ $TEST_DOMAIN -eq "" ]; then
-  read -ep "Are you sure? That domain has no DNS record. If you didn't add that you will have to restart xray and caddy by yourself [y/N]" prompt_response
+  read -ep "Are you sure? That domain has no DNS record. If you didn't add that you will have to restart xray and caddy by yourself [y/N]"$'\n' prompt_response
   if [[ "$prompt_response" =~ ^([yY]) ]]; then
     echo "Ok"
   else 
@@ -30,24 +30,29 @@ if [ $TEST_DOMAIN -eq "" ]; then
   fi
 fi
 
-# Read SSH port
-read -ep "Enter SSH port [default 22]:"$'\n' input_ssh_port
+read -ep "Do you want to install marzban? [y/N] "$'\n' marzban_input
 
-while [ "$input_ssh_port" -eq "80"] || [ "$input_ssh_port" -eq "443" ]; do
-  read -ep "No, ssh can't use $input_ssh_port as port, write again:"$'\n' input_ssh_port
-done
+read -ep "Do you want to configure server security? Do this on first run only. [y/N] "$'\n' configure_ssh_input
+if [[ ${configure_ssh_input,,} == "y" ]]; then
+  # Read SSH port
+  read -ep "Enter SSH port. Default 22, can't use ports: 80, 443 and 4123:"$'\n' input_ssh_port
 
-# Read SSH Pubkey
-read -ep "Enter SSH public key:"$'\n' input_ssh_pbk
-
-echo "$input_ssh_pbk" > ./test_pbk
-ssh-keygen -l -f ./test_pbk
-PBK_STATUS=$(echo $?)
-if [ "$PBK_STATUS" -eq 255 ]; then
-  echo "Can't verify publick key. Try again and be sure to include 'ssh-rsa' or 'ssh-ed25519' and 'user@pcname' at the end of file"
-  exit
+  while [[ "$input_ssh_port" -eq "80" || "$input_ssh_port" -eq "443" || "$input_ssh_port" -eq "4123" ]]; do
+    read -ep "No, ssh can't use $input_ssh_port as port, write again:"$'\n' input_ssh_port
+  done
+  # Read SSH Pubkey
+  read -ep "Enter SSH public key:"$'\n' input_ssh_pbk
+  echo "$input_ssh_pbk" > ./test_pbk
+  ssh-keygen -l -f ./test_pbk
+  PBK_STATUS=$(echo $?)
+  if [ "$PBK_STATUS" -eq 255 ]; then
+    echo "Can't verify publick key. Try again and be sure to include 'ssh-rsa' or 'ssh-ed25519' and 'user@pcname' at the end of file"
+    exit
+  fi
+  rm ./test_pbk
 fi
-rm ./test_pbk
+
+read -ep "Do you want to install WARP and use it on russian websites? [y/N] "$'\n' configure_warp_input
 
 # Check congestion protocol
 if sysctl net.ipv4.tcp_congestion_control | grep bbr; then
@@ -59,96 +64,177 @@ else
     echo "Enabled BBR"
 fi
 
-# Set vars for deb
+yq_install() {
+  wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
+}
+
+yq_install
+
+docker_install() {
+  bash <(wget -qO- https://get.docker.com) @ -o get-docker.sh
+}
+
+if ! command -v docker 2>&1 >/dev/null; then
+    docker_install
+fi
+
+# Generate values for XRay
+export SSH_USER=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 8; echo)
+export SSH_USER_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
+export SSH_PORT=${input_ssh_port:-22}
+export ROOT_LOGIN="yes"
+export IP_CADDY=$(hostname -I | cut -d' ' -f1)
+export CADDY_BASIC_AUTH=$(docker run --rm caddy caddy hash-password --plaintext $SSH_USER_PASS)
+export XRAY_PIK=$(docker run --rm ghcr.io/xtls/xray-core x25519 | head -n1 | cut -d' ' -f 3)
+export XRAY_PBK=$(docker run --rm ghcr.io/xtls/xray-core x25519 -i $XRAY_PIK | tail -1 | cut -d' ' -f 3)
+export XRAY_SID=$(openssl rand -hex 8)
+export XRAY_UUID=$(docker run --rm ghcr.io/xtls/xray-core uuid)
+export XRAY_CFG="/usr/local/etc/xray/config.json"
+
+# Install marzban
+xray_setup() {
+  mkdir -p /opt/xray-vps-setup
+  cd /opt/xray-vps-setup
+  if [[ "${marzban_input,,}" == "y" ]]; then
+    export MARZBAN_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
+    export MARZBAN_PATH=$(openssl rand -hex 8)
+    export MARZBAN_SUB_PATH=$(openssl rand -hex 8)
+    wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/compose | envsubst > ./docker-compose.yml
+    yq eval \
+    '.services.marzban.image = "gozargah/marzban:v0.8.4" |
+     .services.marzban.restart = "always" |
+     .services.marzban.env_file = "./marzban/.env" |
+     .services.marzban.network_mode = "host" | 
+     .services.marzban.volumes[0] = "./marzban_lib:/var/lib/marzban" | 
+     .services.marzban.volumes[1] = "./marzban/xray_config.json:/code/xray_config.json" |
+     .services.marzban.volumes[2] = "./marzban/templates:/var/lib/marzban/templates" |
+     .services.caddy.volumes[2] = "./marzban_lib:/run/marzban"' -i /opt/xray-vps-setup/docker-compose.yml
+    mkdir -p marzban caddy
+    wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/marzban | envsubst > ./marzban/.env
+    mkdir -p /opt/xray-vps-setup/marzban/templates/home
+    wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/confluence_page | envsubst > ./marzban/templates/home/index.html
+    export CADDY_REVERSE="reverse_proxy * unix//run/marzban/marzban.socket"
+    wget -qO- "https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/caddy" | envsubst > ./caddy/Caddyfile
+    wget -qO- "https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/xray" | envsubst > ./marzban/xray_config.json
+  else
+    wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/compose | envsubst > ./docker-compose.yml
+    mkdir -p /opt/xray-vps-setup/caddy/templates
+    yq eval \
+    '.services.xray.image = "ghcr.io/xtls/xray-core:25.1.1" | 
+    .services.xray.restart = "always" | 
+    .services.xray.network_mode = "host" | 
+    .services.caddy.volumes[2] = "./caddy/templates:/srv" |
+    .services.xray.volumes[0] = "./xray:/etc/xray"' -i /opt/xray-vps-setup/docker-compose.yml
+    wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/confluence_page | envsubst > ./caddy/templates/index.html
+    export CADDY_REVERSE="root * /srv
+    file_server"
+    mkdir -p xray caddy
+    wget -qO- "https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/xray" | envsubst > ./xray/config.json
+    wget -qO- "https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/caddy" | envsubst > ./caddy/Caddyfile
+  fi
+}
+
+xray_setup
+
+sshd_edit() {
+  grep -r Port /etc/ssh -l | xargs -n 1 sed -i -e "/Port /c\Port $SSH_PORT"
+  grep -r PasswordAuthentication /etc/ssh -l | xargs -n 1 sed -i -e "/PasswordAuthentication /c\PasswordAuthentication no"
+  grep -r PermitRootLogin /etc/ssh -l | xargs -n 1 sed -i -e "/PermitRootLogin /c\PermitRootLogin no"
+  systemctl restart ssh
+}
+
+add_user() {
+  useradd $SSH_USER
+  usermod -aG sudo $SSH_USER
+  echo $SSH_USER:$SSH_USER_PASS | chpasswd
+  mkdir -p /home/$SSH_USER/.ssh
+  touch /home/$SSH_USER/.ssh/authorized_keys
+  echo $input_ssh_pbk >> /home/$SSH_USER/.ssh/authorized_keys
+  chmod 700 /home/$SSH_USER/.ssh/
+  chmod 600 /home/$SSH_USER/.ssh/authorized_keys
+  chown $SSH_USER:$SSH_USER -R /home/$SSH_USER
+  usermod -aG docker $SSH_USER
+}
+
 debconf-set-selections <<EOF
 iptables-persistent iptables-persistent/autosave_v4 boolean true
 iptables-persistent iptables-persistent/autosave_v6 boolean true
 EOF
 
-# Install Caddy, jq and sudo
-apt-get update
-apt-get install -y debian-keyring debian-archive-keyring apt-transport-https curl iptables-persistent
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --yes --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
-apt-get update
-apt-get install -y caddy jq sudo
-
-# Install XRay
-bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
-
-# Generate values for XRay
-export SSH_USER="xray_user"
-export SSH_USER_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
-export ROOT_USER_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13; echo)
-export SSH_PORT=${input_ssh_port:-22}
-export CADDY_BASIC_AUTH=$(echo $SSH_USER_PASS | caddy hash-password)
-export XRAY_PIK=$(xray x25519 | head -n1 | cut -d' ' -f 3)
-export XRAY_PBK=$(xray x25519 -i $XRAY_PIK | tail -1 | cut -d' ' -f 3)
-export XRAY_SID=$(openssl rand -hex 8)
-export XRAY_UUID=$(xray uuid)
-export IMAGES_CADDY=("IL1.png", "IL2.png", "IL3.png", "SW1.png", "SW2.png", "SW3.png")
-export IMAGE_CADDY=$(printf "%s\n" "${expressions[@]}" | shuf -n1)
-
-# Setup config for Caddy and XRay
-wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/caddy | envsubst > /etc/caddy/Caddyfile
-wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/xray | envsubst > /usr/local/etc/xray/config.json
-
-# Restart XRay and Caddy
-systemctl restart xray
-systemctl restart caddy
-
-# Create user and add to sudoers
-useradd $SSH_USER
-usermod -aG sudo $SSH_USER
-echo $SSH_USER:$SSH_USER_PASS | chpasswd
-
-# Set new password for root user
-echo root:$ROOT_USER_PASS | chpasswd
-
-# Add ssh pbk to authorized_keys
-mkdir -p /home/$SSH_USER/.ssh
-touch /home/$SSH_USER/.ssh/authorized_keys
-echo $input_ssh_pbk >> /home/$SSH_USER/.ssh/authorized_keys
-chmod 700 /home/$SSH_USER/.ssh/
-chmod 600 /home/$SSH_USER/.ssh/authorized_keys
-chown $SSH_USER:$SSH_USER -R /home/$SSH_USER
-
-# Set SSH config 
-wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/ssh_template | envsubst > /etc/ssh/sshd_config
-systemctl restart ssh
-
 # Configure iptables
-iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $SSH_PORT -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-iptables -P INPUT DROP
-netfilter-persistent save
+edit_iptables() {
+  apt-get install iptables-persistent netfilter-persistent -y
+  iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+  iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport $SSH_PORT -j ACCEPT
+  iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT
+  iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT
+  iptables -A INPUT -i lo -j ACCEPT
+  iptables -A OUTPUT -o lo -j ACCEPT
+  iptables -P INPUT DROP
+  netfilter-persistent save
+}
 
-# Print user data
-echo "New user for ssh: $SSH_USER, password for user: $SSH_USER_PASS. New port for SSH: $SSH_PORT. New password for root user: $ROOT_USER_PASS"
-
-# Print outbound and clipboard string
-echo "Clipboard string format"
-echo "vless://$XRAY_UUID@$VLESS_DOMAIN:443?type=tcp&security=reality&pbk=$XRAY_PBK&fp=chrome&sni=$VLESS_DOMAIN&sid=$XRAY_SID&spx=%2F&flow=xtls-rprx-vision" | envsubst
-echo "XRay outbound config"
-wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/xray_outbound | envsubst 
-echo "Sing-box outbound config"
-wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/sing_box_outbound | envsubst 
-echo "Plain data"
-echo "PBK: $XRAY_PBK, SID: $XRAY_SID, UUID: $XRAY_UUID"
+if [[ ${configure_ssh_input,,} == "y" ]]; then
+  sshd_edit
+  add_user
+  edit_iptables
+  echo "New user for ssh: $SSH_USER, password for user: $SSH_USER_PASS. New port for SSH: $SSH_PORT."
+fi
 
 # WARP Install function
-# warp_install() {
-#   curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-#   echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
-#   apt-get update && apt-get install cloudflare-warp -y
-#   
-#   warp-cli registration new
-#   warp-cli mode proxy
-#   warp-cli proxy port 40000
-#   warp-cli connect
-# }
-# 
+warp_install() {
+  apt install gpg -y
+  echo "If this fails then warp won't be added to routing and everything will work without it"
+  curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/cloudflare-client.list
+  apt update 
+  apt install cloudflare-warp -y
+  
+  echo "y" | warp-cli registration new
+  export TRY_WARP=$(echo $?)
+  if [[ $TRY_WARP != 0 ]]; then
+    echo "Couldn't connect to WARP"
+    exit 0
+  else
+    warp-cli mode proxy
+    warp-cli proxy port 40000
+    warp-cli connect
+    if [[ "${marzban_input,,}" == "y" ]]; then
+      export XRAY_CONFIG_WARP="/opt/xray-vps-setup/marzban/xray_config.json"
+    else
+      export XRAY_CONFIG_WARP="/opt/xray-vps-setup/xray/config.json"
+    fi
+    yq eval \
+    '.outbounds[.outbounds | length ] |= . + {"tag": "warp","protocol": "socks","settings": {"servers": [{"address": "127.0.0.1","port": 40000}]}}' \
+    -i $XRAY_CONFIG_WARP
+    yq eval \
+    '.routing.rules[.routing.rules | length ] |= . + {"outboundTag": "warp", "domain": ["geosite:category-ru"]}' \
+    -i $XRAY_CONFIG_WARP
+    docker compose -f /opt/xray-vps-setup/docker-compose.yml down && docker compose -f /opt/xray-vps-setup/docker-compose.yml up -d
+  fi
+}
+
+end_script() {
+  if [[ "${marzban_input,,}" == "y" ]]; then
+    docker run -v /opt/xray-vps-setup/caddy/Caddyfile:/opt/xray-vps-setup/Caddyfile --rm caddy caddy fmt --overwrite /opt/xray-vps-setup/Caddyfile
+    docker compose -f /opt/xray-vps-setup/docker-compose.yml up -d
+    echo "Marzban location: https://$VLESS_DOMAIN/$MARZBAN_PATH. Marzban user: xray_admin, password: $MARZBAN_PASS"
+  else
+    docker run -v /opt/xray-vps-setup/caddy/Caddyfile:/opt/xray-vps-setup/Caddyfile --rm caddy caddy fmt --overwrite /opt/xray-vps-setup/Caddyfile
+    docker compose -f /opt/xray-vps-setup/docker-compose.yml up -d
+    echo "Clipboard string format"
+    echo "vless://$XRAY_UUID@$VLESS_DOMAIN:443?type=tcp&security=reality&pbk=$XRAY_PBK&fp=chrome&sni=$VLESS_DOMAIN&sid=$XRAY_SID&spx=%2F&flow=xtls-rprx-vision" | envsubst
+    echo "XRay outbound config"
+    wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/xray_outbound | envsubst 
+    echo "Sing-box outbound config"
+    wget -qO- https://raw.githubusercontent.com/$GIT_REPO/refs/heads/$GIT_BRANCH/templates_for_script/sing_box_outbound | envsubst 
+    echo "Plain data"
+    echo "PBK: $XRAY_PBK, SID: $XRAY_SID, UUID: $XRAY_UUID"
+  fi
+}
+
+end_script
+set +e
+if [[ ${configure_warp_input,,} == "y" ]]; then
+  warp_install
+fi
